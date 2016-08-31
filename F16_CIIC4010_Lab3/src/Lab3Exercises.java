38865e3536a98236472374f5656e3ee14277ebc1
4015d5 <4015d5@guayaba.ece.uprm.edu>
2016-08-31 14:29:55 -0400

    Added "G." to my name.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Jorge Chevres");
+		JFrame myFrame = new JFrame("Jorge G. Chevres");
 		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 400);
 		myFrame.setSize(200, 200);
